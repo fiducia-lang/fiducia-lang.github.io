4189b70c771cb7b796d7beb9a286166ec106f602
yanliu18 <yanliu18@users.noreply.github.com>
2026-06-02 05:13:38 +0000

    deploy: update language reference fiducia-lang/fic@af9b97f18f69c4a700a34a083ca4e5019e554c91

--- docs/lang-spec/searcher-c2a407aa.js
+++ docs/lang-spec/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ae190c7a.js',
+                path_to_root + 'searchindex-27a2f6de.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
