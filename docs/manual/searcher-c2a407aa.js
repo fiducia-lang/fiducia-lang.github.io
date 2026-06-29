f15518fd64e7857c6089b2edd56b733ac1793f74
yanliu18 <yanliu18@users.noreply.github.com>
2026-06-29 09:21:05 +0000

    deploy: update user manual fiducia-lang/fic@2b64480cbdcd5d45303f705ab1c4a4caf3e59e5e

--- docs/manual/searcher-c2a407aa.js
+++ docs/manual/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c70e74a2.js',
+                path_to_root + 'searchindex-9408a0b5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
